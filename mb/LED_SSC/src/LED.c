03aab25f57beb083ca9bb3cb690bf25951166e71
Tomal-kuet <armanuzzamankuetcse@gmail.com>
2020-10-15 18:48:40 -0400

    LED_SSC

---
+++ mb/LED_SSC/src/LED.c
@@ -0,0 +1,29 @@
+
+
+#include <stdio.h>
+#include "platform.h"
+#include "xil_printf.h"
+#include "xparameters.h"
+#include "xil_exception.h"
+#include "xstatus.h"
+#include "xaxidma.h"
+#include "xil_mem.h"
+#include "util.h"
+#include "xintc.h"
+#include "constants.h"
+#include "sleep.h"
+
+const struct color RED =    {0x01ff, 0x0000, 0x0000};
+const struct color YELLOW = {0x01ff, 0x01ff, 0x0000};
+const struct color GREEN =  {0x0000, 0x01ff, 0x0000};
+const struct color BLUE =   {0x0000, 0x0000, 0x01ff};
+
+int main()
+{
+   // init_platform();
+    u32 *led = (u32*) XPAR_RGB_PWM_0_PWM_AXI_BASEADDR;
+    xil_printf("Hello World\n\r");
+    setLED(led, BLUE);
+    //cleanup_platform();
+    return 1000;
+}
---
+++ mb/LED_SSC/src/LedHeader.h
@@ -0,0 +1,10 @@
+#ifndef __TEST_H_
+#define __TEST_H
+
+void main() __attribute__ ((section (".text.ssc_entry")));
+//int add();
+//int mul(int);
+
+//int (*pfw_add)() = 0x30e0;
+
+#endif
