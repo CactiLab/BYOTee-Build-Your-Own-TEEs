61f827d00380a3679c5c975145993551a4af96d6
Tomal-kuet <armanuzzamankuetcse@gmail.com>
2021-08-26 14:17:39 -0400

    change hmac implementation

--- app1/mb/authentication_tools/hmac.h
+++ app1/mb/authentication_tools/hmac.h
@@ -1,29 +1,129 @@
-#pragma once
-#ifndef PBKDF2_H
-#define PBKDF2_H
-
-
-#include <stddef.h>
-#include <stdint.h>
-
-//below uses SHA2-512 from libsodium. would like to use SHA3-512 at some point but oh well
-#define   SHA1_DIGEST_SIZE  20
-#define SHA512_DIGEST_SIZE  64
-
-#define HASH_BLKSIZE SHA512_DIGEST_SIZE
-#define HASH_OUTSIZE SHA512_DIGEST_SIZE
-#define KEY_IOPAD_SIZE 64
-#define KEY_IOPAD_SIZE128 128
-
-
-#define KDF_OUTSIZE HASH_OUTSIZE //the desired output size of the derived key. equal to hash output size.
-#define KDF_ITER 4096 //this needs to go up alot lol
-#define KDF_SALTSIZE 16 //gets padded, all good
-
 /*
-computes a sha2-512 hmac of <msg> using <key> into <out>
-*/
-void hmac(uint8_t key[HASH_BLKSIZE], const uint8_t* msg, size_t msgsize, uint8_t out[SHA512_DIGEST_SIZE]);
-void hmac_sha1(uint8_t key[HASH_BLKSIZE], const uint8_t* msg, size_t msgsize, uint8_t out[SHA1_DIGEST_SIZE]);
+---------------------------------------------------------------------------
+Copyright (c) 1998-2010, Brian Gladman, Worcester, UK. All rights reserved.
 
-#endif // !PBKDF2_H
+The redistribution and use of this software (with or without changes)
+is allowed without the payment of fees or royalties provided that:
+
+  source code distributions include the above copyright notice, this
+  list of conditions and the following disclaimer;
+
+  binary distributions include the above copyright notice, this list
+  of conditions and the following disclaimer in their documentation.
+
+This software is provided 'as is' with no explicit or implied warranties
+in respect of its operation, including, but not limited to, correctness
+and fitness for purpose.
+---------------------------------------------------------------------------
+Issue Date: 20/12/2007
+
+This is an implementation of HMAC, the FIPS standard keyed hash function
+*/
+
+#ifndef _HMAC2_H
+#define _HMAC2_H
+
+#include <stdlib.h>
+#include <memory.h>
+
+#if defined(__cplusplus)
+extern "C"
+{
+#endif
+
+#if !defined(_SHA1_H)
+#  include "sha1.h"
+#endif
+
+#if !defined(_SHA2_H)
+#  include "sha2.h"
+#endif
+
+#if !defined(_SHA2_H)
+#define HMAC_BLOCK_SIZE      SHA1_BLOCK_SIZE
+#define HMAC_MAX_OUTPUT_SIZE SHA1_DIGEST_SIZE
+#else
+#define HMAC_BLOCK_SIZE      SHA2_MAX_BLOCK_SIZE  
+#define HMAC_MAX_OUTPUT_SIZE SHA2_MAX_DIGEST_SIZE
+#endif
+
+#define HMAC_IN_DATA  0xffffffff
+
+enum hmac_hash  
+{ 
+#ifdef _SHA1_H
+    HMAC_SHA1, 
+#endif
+#ifdef _SHA2_H
+# ifdef SHA_224 
+    HMAC_SHA224, 
+# endif
+# ifdef SHA_256
+    HMAC_SHA256, 
+# endif
+# ifdef SHA_384
+    HMAC_SHA384, 
+# endif
+# ifdef SHA_512
+    HMAC_SHA512, 
+    HMAC_SHA512_256,
+    HMAC_SHA512_224,
+    HMAC_SHA512_192,
+    HMAC_SHA512_128
+# endif
+#endif
+};
+
+typedef VOID_RETURN hf_begin(void*);
+typedef VOID_RETURN hf_hash(const void*, unsigned long len, void*);
+typedef VOID_RETURN hf_end(void*, void*);
+
+typedef struct
+{   hf_begin        *f_begin;
+    hf_hash         *f_hash;
+    hf_end          *f_end;
+    unsigned char   key[HMAC_BLOCK_SIZE];
+    union
+    {
+#ifdef _SHA1_H
+       sha1_ctx    u_sha1;
+#endif
+#ifdef _SHA2_H
+# ifdef SHA_224
+        sha224_ctx  u_sha224;
+# endif
+# ifdef SHA_256
+        sha256_ctx  u_sha256;
+# endif
+# ifdef SHA_384
+        sha384_ctx  u_sha384;
+# endif
+# ifdef SHA_512
+        sha512_ctx  u_sha512;
+# endif
+#endif
+    } sha_ctx[1];
+    unsigned long   input_len;
+    unsigned long   output_len;
+    unsigned long   klen;
+} hmac_ctx;
+
+/* returns the length of hash digest for the hash used  */
+/* mac_len must not be greater than this                */
+int hmac_sha_begin(enum hmac_hash hash, hmac_ctx cx[1]);
+
+int  hmac_sha_key(const unsigned char key[], unsigned long key_len, hmac_ctx cx[1]);
+
+void hmac_sha_data(const unsigned char data[], unsigned long data_len, hmac_ctx cx[1]);
+
+void hmac_sha_end(unsigned char mac[], unsigned long mac_len, hmac_ctx cx[1]);
+
+void hmac_sha(enum hmac_hash hash, const unsigned char key[], unsigned long key_len,
+          const unsigned char data[], unsigned long data_len,
+          unsigned char mac[], unsigned long mac_len);
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif
