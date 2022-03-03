85957ec8336a0cc5460709e248253eba72cb40a0
Tomal-kuet <armanuzzamankuetcse@gmail.com>
2022-03-02 23:01:40 -0500

    add standalone project with timer in hardware for evaluationi

---
+++ Evaluations/Embench-IoT-measurement-microblaze/mb/BYOT_runtime/src/hmac.c
@@ -0,0 +1,76 @@
+
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
+#include "hmac.h"
+#include "sha512.h"
+#include "sha1.h"
+
+#define KDF_USE_HMAC // <- use with length-extension vulnerable hashes (md-based like SHA2)
+
+void hmac_sha1(uint8_t key[HASH_BLKSIZE],
+    const uint8_t* msg, size_t msgsize, uint8_t out[SHA1_DIGEST_SIZE]) {
+    SHA_State context;
+    unsigned char k_ipad[KEY_IOPAD_SIZE];    /* inner padding - key XORd with ipad  */
+    unsigned char k_opad[KEY_IOPAD_SIZE];    /* outer padding - key XORd with opad */
+    int i;
+
+    /* start out by storing key in pads */
+    memset(k_ipad, 0, sizeof(k_ipad));
+    memset(k_opad, 0, sizeof(k_opad));
+    memcpy(k_ipad, key, HASH_BLKSIZE);
+    memcpy(k_opad, key, HASH_BLKSIZE);
+
+    /* XOR key with ipad and opad values */
+    for (i = 0; i < KEY_IOPAD_SIZE; i++) {
+        k_ipad[i] ^= 0x36;
+        k_opad[i] ^= 0x5c;
+    }
+
+    // perform inner SHA
+    SHA_Init(&context);                    /* init context for 1st pass */
+    SHA_Bytes(&context, k_ipad, KEY_IOPAD_SIZE);      /* start with inner pad */
+    SHA_Bytes(&context, msg, msgsize); /* then text of datagram */
+    SHA_Final(&context, out);             /* finish up 1st pass */
+
+    // perform outer SHA
+    SHA_Init(&context);                   /* init context for 2nd pass */
+    SHA_Bytes(&context, k_opad, KEY_IOPAD_SIZE);     /* start with outer pad */
+    SHA_Bytes(&context, out, SHA1_DIGEST_SIZE);     /* then results of 1st hash */
+    SHA_Final(&context, out);          /* finish up 2nd pass */
+}
+
+void hmac(uint8_t key[HASH_BLKSIZE], const uint8_t* msg, size_t msgsize, uint8_t out[SHA512_DIGEST_SIZE]) {
+	SHA512_State state;
+    // uint8_t tmp[SHA512_DIGEST_SIZE];
+    unsigned char k_ipad[KEY_IOPAD_SIZE128];
+    unsigned char k_opad[KEY_IOPAD_SIZE128];
+
+    // return hash(o_key_pad || hash(i_key_pad || message))
+
+    memset(k_ipad, 0, sizeof(k_ipad));
+    memset(k_opad, 0, sizeof(k_opad));
+    memcpy(k_ipad, key, HASH_BLKSIZE);
+    memcpy(k_opad, key, HASH_BLKSIZE);
+
+    //get i_key_pad
+    for (int i = 0; i < KEY_IOPAD_SIZE128; ++i) {
+        k_ipad[i] ^= 0x36;
+        k_opad[i] ^= 0x5c;
+    }
+
+    //get inner hash
+    SHA512_Init(&state);
+    SHA512_Bytes(&state, k_ipad, KEY_IOPAD_SIZE128);
+    SHA512_Bytes(&state, msg, msgsize);
+    SHA512_Final(&state, out);
+
+    //get outer hash
+    SHA512_Init(&state);
+    SHA512_Bytes(&state, k_opad, KEY_IOPAD_SIZE128);
+    SHA512_Bytes(&state, out, SHA512_DIGEST_SIZE);
+
+    // get final output
+    SHA512_Final(&state, out);
+}
+
---
+++ Evaluations/Embench-IoT-measurement-microblaze/tools/createDumpedFiles.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+import argparse, sys, os, struct, hmac
+
+def read_file_content(dumped_command, path_to_dumped_file):
+    print ("Running command : \n")
+    print (dumped_command)
+    os.system(dumped_command)
+    try:
+        f = open(path_to_dumped_file, "rb")
+    except:
+        print("File opening failure")
+        raise
+    data = f.read()
+    f.close()
+    try:
+        os.remove(path_to_dumped_file)
+    except:
+        print("Failed to remove file")
+        raise
+    return data
+
+def main():
+    global abs_SSC_elf_path, dump_command
+    auth_array = [124, 73, 204, 35, 31, 248, 199, 135, 157, 91, 95, 40, 62, 136, 208, 25, 153, 121, 155, 100, 31, 67, 202, 205, 135, 118, 191, 117, 171, 144, 170, 188, 47, 139, 28, 64, 254, 159, 226, 14, 147, 17, 58, 224, 216, 14, 107, 172, 249, 70, 243, 62, 61, 127, 228, 33, 248, 189, 246, 212, 37, 187, 197, 169]
+    auth_key = bytearray(auth_array)
+    
+    parser = argparse.ArgumentParser(description="""
+    The purpose of this script is to create dumped files from executable (elf) files
+    \t this dumped files will run on top of the runtime.
+    """,  formatter_class=argparse.RawTextHelpFormatter)
+
+    parser.add_argument('SSC_elf_path', help="Direcotry of SSC Elf file")
+    args = parser.parse_args()
+
+    abs_SSC_elf_path = os.path.abspath(str(args.SSC_elf_path))
+    if not os.path.exists(abs_SSC_elf_path):
+        print ("SSC Elf file Does not exist")
+    SSC_dump_location = str(os.path.splitext(str(os.path.basename(abs_SSC_elf_path)))[0])
+    #dump_command = 'mb-objcopy -O binary --only-section=.test --only-section=.data --only-section=.rodata ' + abs_SSC_elf_path + ' ' + 'SSC/' + SSC_dump_location 
+    dump_command = 'mb-objcopy -O binary --only-section=.test ' + abs_SSC_elf_path + ' ' + 'SSC/' + SSC_dump_location
+    
+    test_data = read_file_content(dump_command, 'SSC/' + SSC_dump_location)
+    dump_command = 'mb-objcopy -O binary --only-section=.data ' + abs_SSC_elf_path + ' ' + 'SSC/' + SSC_dump_location
+    data = read_file_content(dump_command, 'SSC/' + SSC_dump_location)
+    dump_command = 'mb-objcopy -O binary --only-section=.rodata ' + abs_SSC_elf_path + ' ' + 'SSC/' + SSC_dump_location
+    rodata = read_file_content(dump_command, 'SSC/' + SSC_dump_location)
+
+    try:
+        SSC_file = open('SSC/' + SSC_dump_location, "wb")
+    except:
+        print("Could not open writable file")
+        raise
+
+    test_address = 0x50
+    data_address = 0x13ed4
+    rodata_address = 0x144cc
+    final_data = struct.pack(">i",test_address) + struct.pack(">i",data_address) + struct.pack(">i",rodata_address) + struct.pack(">i",len(test_data)) + struct.pack(">i",len(data)) + struct.pack(">i",len(rodata))
+    
+    final_data = final_data + test_data + data + rodata
+    #print (','.join(format(x, '02x') for x in final_data))
+    m = hmac.new(auth_key, digestmod="sha512")
+    m.update(final_data)
+    ssa_sig = m.digest()
+    #print (','.join(format(x, '02x') for x in auth_key))
+
+    #print (','.join(format(x, '02x') for x in ssa_sig))
+    
+    final_data = ssa_sig + final_data
+    try:
+        SSC_file.write(final_data)
+    except:
+        print("Write to file failed")
+        raise
+    SSC_file.close()
+    os.system("./encrypt_SSA SSC/AES_SSC")
+    print ("\n" + SSC_dump_location + " --dumped file dumped at location -> SSC/")
+
+if __name__ == '__main__':
+    main()
