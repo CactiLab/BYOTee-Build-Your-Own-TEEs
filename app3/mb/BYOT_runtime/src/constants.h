b9a9e07cadc196ad2b026a5d7d5eaab56f3feaf3
Tomal-kuet <armanuzzamankuetcse@gmail.com>
2022-03-07 17:04:34 -0500

    app3: keep state with directive

--- app3/mb/BYOT_runtime/src/constants.h
+++ app3/mb/BYOT_runtime/src/constants.h
@@ -24,6 +24,7 @@
 #define ENC_DEC_DATA_SIZE 64
 #define SIG_LEN 64
 #define AUTH_KEY_SIZE 64
+//#define KEEP_STATE
 
 static const uint8_t auth_key[AUTH_KEY_SIZE] = {124, 73, 204, 35, 31, 248, 199, 135, 157, 91, 95, 40, 62, 136, 208, 25, 153, 121, 155, 100, 31, 67, 202, 205, 135, 118, 191, 117, 171, 144, 170, 188, 47, 139, 28, 64, 254, 159, 226, 14, 147, 17, 58, 224, 216, 14, 107, 172, 249, 70, 243, 62, 61, 127, 228, 33, 248, 189, 246, 212, 37, 187, 197, 169 };
 // LED colors and controller
@@ -34,6 +35,21 @@ struct color
     u32 b;
 };
 
+#ifdef KEEP_STATE
+
+enum commands
+{
+	LOAD_CODE,
+	QUERY_DRM,
+	SSC_COMMAND,
+	EXIT,
+	EXECUTE,
+	SAVE,
+	RELOAD
+};
+#endif
+
+#ifndef KEEP_STATE
 enum commands
 {
     LOAD_CODE,
@@ -44,6 +60,7 @@ enum commands
     PREEXEATT,
     POSTEXEATT
 };
+#endif
 enum states
 {
     STOPPED,
