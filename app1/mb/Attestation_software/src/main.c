5cc6f88427d607cf713ccb59ec6c0a10f397bb2e
Tomal-kuet <armanuzzamankuetcse@gmail.com>
2022-08-04 18:06:20 -0400

    update memory map:att

--- app1/mb/Attestation_software/src/main.c
+++ app1/mb/Attestation_software/src/main.c
@@ -1,19 +1,20 @@
-#include<aes.h>
-hw_att_md current_att_md;
+#include "aes.h"
+#include "constants.h"
+volatile hw_att_md *current_att_md = (hw_att_md *)ATT_BASE;
 volatile SSA_loader *loader = (SSA_loader *)SSA_BASE;
 
 int main()
 {
 	while (1)
 	{
-		while(current_att_md.cmd == 0);
-		if (current_att_md.cmd  == 0xA)
+		while(current_att_md->cmd == 0);
+		if (current_att_md->cmd  == 0xA)
 		{
 			struct AES_ctx ctx;
 			AES_init_ctx_iv(&ctx, AES_CBC_key, AES_CBC_IV);
 
-			int length = current_att_md.ssa_size/AES_BLOCK_SIZE + 1;
-			int rem = current_att_md.ssa_size;
+			int length = current_att_md->ssa_size/AES_BLOCK_SIZE + 1;
+			int rem = current_att_md->ssa_size;
 
 			for (int i = 0; i < length; i++)
 			{
@@ -21,7 +22,7 @@ int main()
 				rem = rem - AES_BLOCK_SIZE;
 			}
 		}
-		else if (current_att_md.cmd == 0xD)
+		else if (current_att_md->cmd == 0xD)
 		{
 
 		}
