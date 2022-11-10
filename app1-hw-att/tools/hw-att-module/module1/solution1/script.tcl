a01fd91dbe43b1744d9b5e19bccba52369c52666
sbhakim <sbhakim@kth.se>
2022-11-10 14:34:41 -0500

    initial-project

---
+++ app1-hw-att/tools/hw-att-module/module1/solution1/script.tcl
@@ -0,0 +1,17 @@
+############################################################
+## This file is generated automatically by Vivado HLS.
+## Please DO NOT edit it.
+## Copyright (C) 1986-2017 Xilinx, Inc. All Rights Reserved.
+############################################################
+open_project module1
+set_top blake2s
+add_files blake2s.c
+add_files blake2s.h
+open_solution "solution1"
+set_part {xc7z007sclg400-2} -tool vivado
+create_clock -period 10 -name default
+#source "./module1/solution1/directives.tcl"
+#csim_design
+csynth_design
+#cosim_design
+export_design -format ip_catalog
---
+++ app1-hw-att/tools/hw-att-module/module1/vivado_hls.app
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<project xmlns="com.autoesl.autopilot.project" top="blake2s">
+  <files>
+    <file name="blake2s.c" sc="0" tb="false" cflags=""/>
+    <file name="blake2s.h" sc="0" tb="false" cflags=""/>
+  </files>
+  <solutions>
+    <solution name="solution1" status="active"/>
+  </solutions>
+  <includePaths/>
+  <libraryPaths/>
+  <Simulation>
+    <SimFlow name="csim"/>
+  </Simulation>
+</project>
